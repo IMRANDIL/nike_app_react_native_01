a968ad1aebbbfc647eb4f58c94ffe0faa918f5bf
IMRANDIL <aliimranadil2@gmail.com>
2023-02-11 20:13:55 +0530

    some more

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProductDetailsScreen from './screens/ProductDetailsScreen';
 import ProductsScreen from './screens/ProductsScreen';
 import ShopingCart from './screens/ShopingCart';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
@@ -14,7 +15,14 @@ const Navigation = () => {
         <Stack.Screen
           name="Products"
           component={ProductsScreen}
-          options={{headerTitleAlign: 'center'}}
+          options={{
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <Pressable>
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+              </Pressable>
+            ),
+          }}
         />
         <Stack.Screen
           name="Product Details"
