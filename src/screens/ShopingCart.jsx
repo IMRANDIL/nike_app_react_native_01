a3ccf810baad587a5ae64e44bf668c9c0b1f0d10
IMRANDIL <aliimranadil2@gmail.com>
2023-02-11 18:33:29 +0530

    some more change

--- src/screens/ShopingCart.jsx
+++ src/screens/ShopingCart.jsx
@@ -1,4 +1,4 @@
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import React from 'react';
 import CartListItem from '../components/CartListItem';
 import cart from '../data/cart';
@@ -7,9 +7,22 @@ const ShopingCart = () => {
   return (
     <FlatList
       data={cart}
+      keyExtractor={(item, index) => item + index}
       renderItem={({item}) => <CartListItem cartItem={item} />}
+      ListFooterComponent={() => (
+        <View style={styles.totalContainer}>
+          <View style={styles.row}>
+            <Text>Subtotal</Text>
+            <Text>410.00</Text>
+          </View>
+        </View>
+      )}
     />
   );
 };
 
 export default ShopingCart;
+
+const styles = StyleSheet.create({
+  totalContainer: {},
+});
