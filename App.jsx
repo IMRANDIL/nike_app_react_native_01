0ecab9460b59ddc6ffe3750b1a800a6d68c0668c
IMRANDIL <aliimranadil2@gmail.com>
2023-02-11 18:04:15 +0530

    some more  changes

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import {StyleSheet, View} from 'react-native';
 import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 import ProductsScreen from './src/screens/ProductsScreen';
+import ShopingCart from './src/screens/ShopingCart';
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ProductsScreen /> */}
-      <ProductDetailsScreen />
+      {/* <ProductDetailsScreen /> */}
+      <ShopingCart />
     </View>
   );
 }
