35c24d4e825ecd4e511473579946ebd8690bab27
IMRANDIL <aliimranadil2@gmail.com>
2023-02-11 19:26:27 +0530

    some more change

--- App.jsx
+++ App.jsx
@@ -1,13 +1,12 @@
 import {StyleSheet, View} from 'react-native';
+import Navigation from './src/Navigation';
 import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 import ProductsScreen from './src/screens/ProductsScreen';
 import ShopingCart from './src/screens/ShopingCart';
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ProductsScreen /> */}
-      {/* <ProductDetailsScreen /> */}
-      <ShopingCart />
+      <Navigation />
     </View>
   );
 }
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,14 +2,16 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import ProductDetailsScreen from './screens/ProductDetailsScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShopingCart from './screens/ShopingCart';
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={ProductsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
