40bf88fe6641f5d66e0a8c1ca55079424f9b1cff
IMRANDIL <aliimranadil2@gmail.com>
2023-02-11 13:50:12 +0530

    some more change done

--- src/screens/ProductDetailsScreen.jsx
+++ src/screens/ProductDetailsScreen.jsx
@@ -6,30 +6,39 @@ import {
   FlatList,
   Dimensions,
   ActivityIndicator,
+  ScrollView,
 } from 'react-native';
 import React from 'react';
 import products from '../data/products';
 
 const {width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 const ProductDetailsScreen = () => {
   const product = products[0];
 
   return (
     <View>
-      <FlatList
-        data={product.images}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({item}) =>
-          !item ? (
-            <ActivityIndicator />
-          ) : (
-            <Image source={{uri: item}} style={styles.image} />
-          )
-        }
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-      />
+      <ScrollView style={{padding: 20}} showsVerticalScrollIndicator={false}>
+        <FlatList
+          data={product.images}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({item}) =>
+            !item ? (
+              <ActivityIndicator />
+            ) : (
+              <Image source={{uri: item}} style={styles.image} />
+            )
+          }
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+        />
+        <View style={{paddingBottom: 30}}>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -40,5 +49,21 @@ const styles = StyleSheet.create({
   image: {
     width: width,
     aspectRatio: 1,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: '500',
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: '500',
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: '300',
   },
 });
