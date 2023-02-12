9434b0aef476afd93d99e6172b3d141421a59259
IMRANDIL <aliimranadil2@gmail.com>
2023-02-12 11:13:54 +0530

    cart slice added

---
+++ src/store/cartSlice.js
@@ -0,0 +1,10 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialState,
+});
