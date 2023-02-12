24579947074c682b3be38740e91d5a3831acbca4
IMRANDIL <aliimranadil2@gmail.com>
2023-02-12 11:19:30 +0530

    some more slice done

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,10 +1,22 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  cart: [],
+  items: [],
+  deliveryFee: 15,
+  freeDeliveryFrom: 200,
 };
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: initialState,
+  reducers: {
+    addCartItem: (state, action) => {
+      const newProduct = action.payload.product;
+      state.items.push({
+        product: newProduct,
+        quantity: 1,
+      });
+    },
+    changeQuantity: (state, action) => {},
+  },
 });
